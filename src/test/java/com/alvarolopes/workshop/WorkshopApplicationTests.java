9a8af97bf7a74db9c243d3e64c05c2f5d0ea7aa4
alvimlopes <alvim.lopez@gmail.com>
2021-09-15 19:29:37 -0300

    primeiro comit - projeto criado

---
+++ src/test/java/com/alvarolopes/workshop/WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvarolopes.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
